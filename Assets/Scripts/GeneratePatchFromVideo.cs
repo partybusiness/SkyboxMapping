af01ae44a4464011fad079ba317f52f2a5db7676
R Hill <rrh@horriblepain.com>
2019-08-03 15:47:29 -0500

    Can generate a series of patch images from a 360 video

---
+++ Assets/Scripts/GeneratePatchFromVideo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(CreatePanoramaPatch))]
+public class GeneratePatchFromVideo : MonoBehaviour {
+
+    [SerializeField]
+    VideoPlayer player;
+
+    [SerializeField]
+    string patchName = "patch_{0}";
+
+    CreatePanoramaPatch patchMaker;
+
+	void Start () {
+        Time.captureFramerate = Mathf.RoundToInt((float)player.clip.frameRate);
+	}
+	
+	void Update () {
+		if (player.isPlaying)
+        {
+            if (patchMaker == null)
+                patchMaker = GetComponent<CreatePanoramaPatch>();
+            //player.frame
+            patchMaker.patchName = string.Format(patchName, player.frame);
+        }
+        ScreenCapture.CaptureScreenshotAsTexture();
+    }
+}
